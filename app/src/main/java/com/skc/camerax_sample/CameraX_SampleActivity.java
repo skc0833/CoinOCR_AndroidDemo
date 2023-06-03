f9a9a199133dd96f7baa8fae488c7570b8a48e97
Shin Kwang Chul <skc0833@gmail.com>
2023-06-03 23:08:19 +0900

    카메라 저장 기능(takePicture) 추가

--- app/src/main/java/com/skc/camerax_sample/CameraX_SampleActivity.java
+++ app/src/main/java/com/skc/camerax_sample/CameraX_SampleActivity.java
@@ -1,11 +1,14 @@
 package com.skc.camerax_sample;
 
+import android.content.ContentValues;
 import android.content.pm.PackageManager;
-import android.os.Build;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +16,6 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
@@ -23,13 +25,9 @@ import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.lifecycle.LifecycleOwner;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -106,6 +104,63 @@ public class CameraX_SampleActivity extends AppCompatActivity {
     }, ContextCompat.getMainExecutor(this));
   }
 
+  private String getPathFromUri(Uri uri){
+    Cursor cursor = getContentResolver().query(uri, null, null, null, null );
+    cursor.moveToNext();
+    int idx = cursor.getColumnIndex("_data");
+    String path = "";
+    if (idx >= 0) {
+      path = cursor.getString(idx);
+    } else {
+      assert false;
+    }
+    cursor.close();
+    return path;
+  }
+
+  private void takePicture() {
+    String filename = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date())+ ".jpg";
+    ContentValues contentValues = new ContentValues();
+    contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, filename);
+    contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+    contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES + "/skc"); // 내장 메모리\Pictures\skc
+    mImageCapture.takePicture(
+        new ImageCapture.OutputFileOptions.Builder(
+          getContentResolver(),
+          MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+          contentValues
+        ).build(),
+        mExecutor,
+        new ImageCapture.OnImageSavedCallback() {
+          @Override
+          public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
+            // e.g., outputFileResults.getSavedUri() = content://media/external/images/media/88296
+            Log.d(TAG, "onImageSaved getSavedUri=" + outputFileResults.getSavedUri());
+            // e.g., path=/storage/emulated/0/Pictures/skc/20230603_224115.jpg
+            String path = getPathFromUri(outputFileResults.getSavedUri());
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+              @Override
+              public void run() {
+                Toast.makeText(CameraX_SampleActivity.this, path + " Saved!", Toast.LENGTH_SHORT).show();
+              }
+            });
+          }
+
+          @Override
+          public void onError(ImageCaptureException e) {
+            Log.e(TAG, " takePicture() failed! error=" + e.toString());
+            e.printStackTrace();
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+              @Override
+              public void run() {
+                Toast.makeText(CameraX_SampleActivity.this, "Error takePicture() " + e, Toast.LENGTH_SHORT).show();
+              }
+            });
+          }
+        }
+    );
+  }
+
   private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
     Preview preview = new Preview.Builder().build();
     CameraSelector cameraSelector = new CameraSelector.Builder()
@@ -119,77 +174,16 @@ public class CameraX_SampleActivity extends AppCompatActivity {
             .build();
     //skc 원래는 mPreviewView.createSurfaceProvider() 였는데 빌드 에러로 변경함(없는 method)
     preview.setSurfaceProvider(mPreviewView.getSurfaceProvider());
-    Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector,  mImageCapture, imageAnalysis, preview);
+    cameraProvider.bindToLifecycle(this, cameraSelector,  mImageCapture, imageAnalysis, preview);
 
     Button captureButton = findViewById(R.id.image_capture_button);
     captureButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        // e.g, /data/user/0/com.skc.camerax_sample/files/skc_capture/20230422004802.jpg
-        String filename = new SimpleDateFormat("yyyyMMddHHmmss", Locale.US).format(new Date())+ ".jpg";
-        File save_file = new File(getImageCaptureSaveDirPath(), filename);
-
-        ImageCapture.OutputFileOptions outputFileOptions =
-          new ImageCapture.OutputFileOptions.Builder(save_file).build();
-
-        mImageCapture.takePicture(
-          outputFileOptions,
-          mExecutor,
-          new ImageCapture.OnImageSavedCallback() {
-            @Override
-            public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
-              // e.g, getSavedUri=file:///data/user/0/com.skc.camerax_sample/files/skc_capture/20230422004802.jpg
-              // adb shell 에서 ls /data 는 권한이 없어 실패한다(ls /sdcard 는 성공함)
-              // 윈도우 탐색기에서 내 PC\신광철의 Note10+\내장 메모리\Android\data\com.skc.camerax_sample\files\skc_capture 에 가끔 접근 가능함
-              Log.d(TAG, "onImageSaved getSavedUri=" + String.valueOf(outputFileResults.getSavedUri()));
-
-              if (true) {
-                String target_filename = save_file.getName();
-                File target_file = new File(getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), target_filename);
-                //File target_file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES), target_filename);
-                // --> Operation not permitted IOException 발생함(/storage/emulated/0/Movies/20230422112214.jpg)
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                  try {
-                    // copy from /data/user/0/com.skc.camerax_sample/files/skc_capture/20230422111621.jpg
-                    // to /storage/emulated/0/Android/data/com.skc.camerax_sample/files/Pictures/20230422111621.jpg
-                    // --> IOException 은 발생하지 않지만 해당 폴더 확인해보면 실제 복사는 이뤄지지 않았음
-                    Log.d(TAG, "copy from " + save_file.toPath() + " to " + target_file.toPath());
-                    Files.copy(save_file.toPath(), target_file.toPath());
-                  } catch (IOException e) {
-                    throw new RuntimeException(e);
-                  }
-                }
-              }
-              new Handler(Looper.getMainLooper()).post(new Runnable() {
-                @Override
-                public void run() {
-                  Toast.makeText(CameraX_SampleActivity.this, save_file.toString() + " Saved!", Toast.LENGTH_SHORT).show();
-                }
-              });
-            }
-
-            @Override
-            public void onError(ImageCaptureException e) {
-              Log.e(TAG, " takePicture() failed! error=" + e.toString());
-              e.printStackTrace();
-            }
-          });
+        // mImageCapture.takePicture() 함수로 Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getPath(); 에 저장시
+        // androidx.camera.core.ImageCaptureException: Failed to write temp file 에러 발생중
+        takePicture();
       }
     });
-  }
-
-  private String getImageCaptureSaveDirPath() {
-    // getExternalStoragePublicDirectory() 사용시 권한 오류(EPERM (Operation not permitted))
-    // Android 10 (API level 29) 부터 scoped access into external storage 라서 발생
-    // File dir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES));
-    // getExternalFilesDir(Environment.DIRECTORY_PICTURES) 사용시에는 takePicture() 시에 Failed to write temp file 에러
-    File dir = new File(getApplicationContext().getFilesDir(), "skc_capture");
-    if (!dir.exists()) {
-      Log.d(TAG, "getImageCaptureSaveDirPath() mkdir " + dir.toString());
-      if (!dir.mkdirs()) {
-        Log.e(TAG, "getImageCaptureSaveDirPath() mkdir failed! " + dir.toString());
-      }
-    }
-    return dir.toString();
   }
 }
